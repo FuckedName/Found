6017b0e9b58c87cdd8d3cfe1507cee92c0d64adc
JasonRen@qq.com <JasonRen@qq.com>
2020-10-05 09:30:39 +0800

    update config.txt

--- Install.sh
+++ Install.sh
@@ -1,4 +1,5 @@
 EXECUTE_FILE_NAME="Found"
+CONFIG_FILE_NAME="config.txt"
 EXECUTE_PATH_NAME="/usr/bin"
 
 
@@ -10,6 +11,7 @@ rm -rf ${EXECUTE_PATH_NAME}/${EXECUTE_FILE_NAME}
 gcc -g -rdynamic -o ${EXECUTE_FILE_NAME} main.c  -std=c99 -lpthread
 
 cp -rf ${EXECUTE_FILE_NAME} ${EXECUTE_PATH_NAME}/${EXECUTE_FILE_NAME}
+cp -rf ${CONFIG_FILE_NAME} ${EXECUTE_PATH_NAME}
 
 ${EXECUTE_FILE_NAME}
 
